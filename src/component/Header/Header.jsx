c3e41db9a016012abe389dab4d4f783a46dcc8e8
rajalishikhe <nahidujjamanraj@gmail.com>
2025-03-26 15:07:41 +0600

    Learning about useNavigation

--- src/component/Header/Header.jsx
+++ src/component/Header/Header.jsx
@@ -1,20 +1,20 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 import './style.css';
 
 const Header = () => {
     return (
         <div>
             <h2>This is my Home page</h2>
-            <ul >
+            <nav >
                 
-                <Link className='style1' to={'/'}>Home</Link>
-                <Link className='style1' to={'/Data'}>Data</Link>
-                <Link className='style1' to={'/Contract'}>Contract</Link>
-                <Link className='style1' to={'/About'}>About</Link>
-                <Link className='style1' to={'/Users'}>Users</Link>
-                <Link className='style1' to={'/Post'}>Post</Link>
-            </ul>
+                <NavLink  to={'/'}>Home</NavLink>
+                <NavLink  to={'/Data'}>Data</NavLink>
+                <NavLink  to={'/Contract'}>Contract</NavLink>
+                <NavLink  to={'/About'}>About</NavLink>
+                <NavLink  to={'/Users'}>Users</NavLink>
+                <NavLink  to={'/Post'}>Post</NavLink>
+            </nav>
             
         </div>
     );
--- src/component/Home/Home.jsx
+++ src/component/Home/Home.jsx
@@ -1,13 +1,21 @@
 import React from 'react';
-import { Outlet } from 'react-router-dom';
+import { Outlet, useNavigation } from 'react-router-dom';
 import Footer from '../Footer/Footer';
 import Header from '../Header/Header';
 
 const Home = () => {
+    const navigation=useNavigation()
+    console.log(navigation)
     return (
         <div>
-            <Header></Header>
-            <Outlet></Outlet>
+             <Header></Header>
+            {
+                navigation.state==='loading'?
+                <p>Loading</p>:<Outlet></Outlet>
+
+            }
+           
+            
             <Footer></Footer>
             
             
